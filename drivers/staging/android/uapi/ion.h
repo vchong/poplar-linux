323ba9db1103e9995c51f1c100f2827dbc211b3f
Etienne Carriere <etienne.carriere@linaro.org>
2017-03-23 14:02:44 +0100

    ion: "unmapped" heap for secure data path **not for mainline**
    OP-TEE/SDP (Secure Data Path) memory pools are created through ION
    secure type heap" from Allwinner. This change renames "secure" into
    "unmapped" as, from Linux point of view, the heap constraint is
    manipulating unmapped memory pools/buffers.
    
    "Unmapped" heap support is integrated in ION UAPI (actually this was
    the Allwinner initial proposal) and ION DT parsing support.
    
    Based in work from Sunny <sunny@allwinnertech.com> for Allwinner.
    
    Signed-off-by: Etienne Carriere <etienne.carriere@linaro.org>
    Reviewed-by: Joakim Bech <joakim.bech@linaro.org>
    (cherry picked from commit 4a95713514ddc3d55d5df213513aeec5a3717243)
    Signed-off-by: Victor Chong <victor.chong@linaro.org>

--- drivers/staging/android/uapi/ion.h
+++ drivers/staging/android/uapi/ion.h
@@ -32,6 +32,8 @@ typedef int ion_user_handle_t;
  *				 carveout heap, allocations are physically
  *				 contiguous
  * @ION_HEAP_TYPE_DMA:		 memory allocated via DMA API
+ * @ION_HEAP_TYPE_UNMAPPED:	 memory not intended to be mapped into the
+ *				 linux address space unless for debug cases
  * @ION_NUM_HEAPS:		 helper for iterating over heaps, a bit mask
  *				 is used to identify the heaps, so only 32
  *				 total heap types are supported
@@ -46,6 +48,7 @@ enum ion_heap_type {
 	/* backwards compatible with v3.4 ion */
 	ION_HEAP_ID_CMA = 15, /* cma: ddr */
 	ION_HEAP_ID_TEE_SEC_MEM, /* secure mem in TEE */
+	ION_HEAP_TYPE_UNMAPPED,
 	ION_HEAP_TYPE_CUSTOM, /*
 			       * must be last so device specific heaps always
 			       * are at the end of this enum
